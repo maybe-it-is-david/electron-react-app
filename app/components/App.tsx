4a073281cfd4ba8152798e29a9bbf43425792bb3
David Barton <david.z.barton@gmail.com>
2025-03-30 09:56:31 -0500

    adding shadcn and dark/light mode theme provider

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,6 +1,17 @@
-import WelcomeKit from '@/lib/welcome/WelcomeKit'
+// import WelcomeKit from '@/lib/welcome/WelcomeKit'
 import '../styles/app.css'
+import { Button } from './ui/button'
+import { useState } from 'react'
+import { ThemeToggle } from './theme-toggle'
 
 export default function App() {
-  return <WelcomeKit />
+  const [count, setCount] = useState(0)
+  // return <WelcomeKit />
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <Button onClick={() => setCount((x) => x + 1)}>Increment</Button>
+      <ThemeToggle />
+    </div>
+  )
 }
