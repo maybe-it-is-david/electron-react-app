94a233eb1d8c4095e071fad5352dc3099c7de432
David Barton <david.z.barton@gmail.com>
2025-03-30 10:28:12 -0500

    chat interface starter from v0

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -3,15 +3,22 @@ import '../styles/app.css'
 import { Button } from './ui/button'
 import { useState } from 'react'
 import { ThemeToggle } from './theme-toggle'
+import ChatInterface from './chat-interface'
+import Layout from './layout'
 
 export default function App() {
   const [count, setCount] = useState(0)
-  // return <WelcomeKit />
   return (
-    <div>
-      <div>Count: {count}</div>
-      <Button onClick={() => setCount((x) => x + 1)}>Increment</Button>
-      <ThemeToggle />
-    </div>
+    <Layout>
+      <ChatInterface />
+    </Layout>
   )
+  // return <WelcomeKit />
+  // return (
+  //   <div>
+  //     <div>Count: {count}</div>
+  //     <Button onClick={() => setCount((x) => x + 1)}>Increment</Button>
+  //     <ThemeToggle />
+  //   </div>
+  // )
 }
